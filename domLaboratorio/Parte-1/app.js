bcd7a9079d4cf280a8911e9f4f714f2b70520451
Brian Marulanda <b.marulanda.h02@gmail.com>
2026-07-02 11:39:15 -0500

    Agrego funciones a las imagenes y al boton

--- domLaboratorio/Parte-1/app.js
+++ domLaboratorio/Parte-1/app.js
@@ -1,4 +1,16 @@
-let img1=document.querySelector("#img1");
-let img2=document.querySelector("#img2");
-let img3=document.querySelector("#img3");
-let boton=document.querySelector("#recargarButton");
+let img1 = document.querySelector('#gato');
+let img2 = document.querySelector('#perro');
+let img3 = document.querySelector('#loro');
+let boton = document.querySelector('#recargarButton');
+img1.addEventListener('mouseover', () => {
+	img1.src = './img/perro.png';
+});
+img2.addEventListener('mouseover', () => {
+	img2.src = './img/calavera.png';
+});
+img3.addEventListener('mouseover', () => {
+	img3.src = './img/perro.png';
+});
+boton.addEventListener('click', () => {
+    location.reload();
+})
